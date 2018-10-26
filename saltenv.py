d17527ab279c17ad60f6eb86db37ded969a55a97
Yogesh Bhoopathy <yogeshbhoopathy@gmail.com>
2018-10-25 20:59:49 -0700

    The prime commit

---
+++ saltenv.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# Check if Vagranfile exists in the current directory
+# Create a tmp directory if it exists delete recreate the dir
+	# Add a command line flag to not delete tmp
+# Get all minion hostnames as command line arguments
+# Create minion specific files under tmp directory
+	# Create /etc/hosts file under each minion directory
+# Set a private IP for salt master
+
